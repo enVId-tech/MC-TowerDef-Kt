fce4fa59cc430d4f84c8ce5fb7aa32e1750dd668
Erick Tran <thelittlebotengineer@gmail.com>
2025-10-20 00:00:25 -0700

    feat(main): add base logging code to verify functionality

--- src/main/kotlin/dev/etran/towerDefMc/TowerDefMC.kt
+++ src/main/kotlin/dev/etran/towerDefMc/TowerDefMC.kt
@@ -5,9 +5,15 @@ import org.bukkit.plugin.java.JavaPlugin
 class TowerDefMC : JavaPlugin() {
     override fun onEnable() {
         // Plugin startup logic
+        logger.info {
+            "Hello World!"
+        }
     }
 
     override fun onDisable() {
         // Plugin shutdown logic
+        logger.info {
+            "Goodbye World!"
+        }
     }
 }
