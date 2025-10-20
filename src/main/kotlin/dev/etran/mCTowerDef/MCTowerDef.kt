fbc7399f0b776af8da78995f848467a83ae0720d
Erick Tran <thelittlebotengineer@gmail.com>
2025-10-19 22:53:58 -0700

    Initial commit

---
+++ src/main/kotlin/dev/etran/mCTowerDef/MCTowerDef.kt
@@ -0,0 +1,14 @@
+package dev.etran.mCTowerDef
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class MCTowerDef : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
