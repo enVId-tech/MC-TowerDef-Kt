39b3ec46e68d62694a42e46f15c8d82f3725eb33
Erick Tran <thelittlebotengineer@gmail.com>
2025-10-19 23:27:37 -0700

    refactor(project): rename project package

--- src/main/kotlin/dev/etran/towerDefMc/TowerDefMC.kt
+++ src/main/kotlin/dev/etran/towerDefMc/TowerDefMC.kt
@@ -1,9 +1,8 @@
-package dev.etran.mCTowerDef
+package dev.etran.towerDefMc
 
 import org.bukkit.plugin.java.JavaPlugin
 
-class MCTowerDef : JavaPlugin() {
-
+class TowerDefMC : JavaPlugin() {
     override fun onEnable() {
         // Plugin startup logic
     }
